d7418aa0ca25a6d74809f2182b74d58c6b75b8d2
rouhuhgrruagwauwarwbogwuo <214204@clvsc.tyc.edu.tw>
2025-02-13 14:05:21 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,39 @@
-from flask import Flask
+from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+import os
 
-# 初始化 Flask 應用
 app = Flask(__name__)
 
-# 定義首頁路由
-@app.route('/')
-def hello_world():
-    return '吳承諺'
+# 設定 LINE Channel Access Token 和 Secret
+LINE_CHANNEL_ACCESS_TOKEN = os.getenv("LINE_CHANNEL_ACCESS_TOKEN")
+LINE_CHANNEL_SECRET = os.getenv("LINE_CHANNEL_SECRET")
 
-# 啟動應用
-if __name__ == '__main__':
-    app.run(debug=True)
+line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
+line_handler = WebhookHandler(LINE_CHANNEL_SECRET)
+
+@app.route("/callback", methods=["POST"])
+def callback():
+    signature = request.headers["X-Line-Signature"]
+    body = request.get_data(as_text=True)
+
+    try:
+        line_handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return "OK"
+
+@line_handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    user_message = event.message.text
+    reply_message = f"你說了：{user_message}"
+    
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=reply_message)
+    )
+
+if __name__ == "__main__":
+    app.run(port=8000)
